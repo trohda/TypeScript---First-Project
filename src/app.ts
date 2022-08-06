a91a28edd1118cef7cc6cd3d7adc7735a8759d40
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-06 18:32:49 +0200

    Use getter

--- src/app.ts
+++ src/app.ts
@@ -163,6 +163,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class projectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -175,8 +183,7 @@ class projectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
