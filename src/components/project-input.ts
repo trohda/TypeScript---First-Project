64c3fb1fe817f83867b16c0a5b6d3ded50533ec4
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-11 12:56:36 +0200

    Import names update

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,10 +1,10 @@
-import { autoBind } from "../decorators/autobind.js";
+import { autoBind as Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
-import { Validatable, validate } from "../util/validation.js";
-import { Component } from "./base-component.js";
+import * as Validation from "../util/validation.js";
+import Cmp from "./base-component.js";
 
 // ProjectInput Class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
@@ -39,18 +39,18 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
 
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
 
-    const peopleValidatable: Validatable = {
+    const peopleValidatable: Validation.Validatable = {
       value: +enteredPeople,
       required: true,
       min: 1,
@@ -58,9 +58,9 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     };
 
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(peopleValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(peopleValidatable)
     ) {
       alert("Provide valid input");
       return;
@@ -69,7 +69,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     }
   }
 
-  @autoBind
+  @Autobind
   private submitHandler(this: ProjectInput, event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
 import { autoBind } from "../decorators/autobind.js";
 import { Draggable } from "../models/drag-drop.js";
 import { Project } from "../models/project.js";
-import { Component } from "./base-component.js";
+import Component from "./base-component.js";
 
 // ProjectItem Class
 export class ProjectItem
