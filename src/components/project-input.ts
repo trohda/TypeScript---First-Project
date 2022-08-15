e8941e8afc8b08966c810a751a8500ee151d3161
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-15 10:28:47 +0200

    Add Typescript support to webpack

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { autoBind as Autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import * as Validation from "../util/validation.js";
-import Cmp from "./base-component.js";
+import { autoBind as Autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import * as Validation from "../util/validation";
+import Cmp from "./base-component";
 
 // ProjectInput Class
 export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
