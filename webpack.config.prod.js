3515df592dade61bdd067c7347909bc61efcebb7
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-15 13:57:45 +0200

    Webpack production configuration

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,14 @@
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
-  devtool: "inline-source-map",
+
   module: {
     rules: [
       {
@@ -26,4 +26,5 @@ module.exports = {
       directory: path.join(__dirname, "./"),
     },
   },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
